204628209679b04d59845c6b7b19abb525390ff2
VeronicaLA <llaverito003@gmail.com>
2015-07-31 19:30:45 -0500

    v

---
+++ Nuskin/www/js/contenidoiframe.js
@@ -0,0 +1,19 @@
+	function edit() {
+		document.all("frame1").src="http://www.intellibanks.net/alkimia/bin/edit/NuSkinTemplete/WebHome"; 
+		}
+	function attach() {
+		document.all("frame1").src="http://www.intellibanks.net/alkimia/bin/attach/NuSkinTemplete/WebHome"; 
+		}
+	function view() {
+		document.all("frame1").src="http://www.intellibanks.net/alkimia/bin/view/NuSkinTemplete/WebHome?raw=on"; 
+		}
+	function rename() {
+		document.all("frame1").src="http://www.intellibanks.net/alkimia/bin/rename/NuSkinTemplete/WebHome"; 
+		}
+	function eliminar() {
+		document.all("frame1").src="http://www.intellibanks.net/alkimia/bin/rename/NuSkinTemplete/WebHome"; 
+		}
+	function create() {
+		document.all("frame1").src="http://www.intellibanks.net/alkimia/bin/view/NuSkinTemplete/WebTopicCreator"; 
+		}
+		
